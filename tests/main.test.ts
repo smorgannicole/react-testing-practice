ef95af7dd524864b77b76530f3488e44d33f56b0
Morgan <smorgannicole@gmail.com>
2024-06-26 11:34:17 -0500

    setting up vitest

---
+++ tests/main.test.ts
@@ -0,0 +1,8 @@
+import { it, expect, describe } from "vitest";
+
+// create test suite to make sure setup was successful
+describe("group", () => {
+  it("should", () => {
+    expect(1).toBeTruthy();
+  });
+});
