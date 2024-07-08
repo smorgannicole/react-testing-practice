21176bf05105036b08d885f6650f96e30e68c7d9
Morgan <smorgannicole@gmail.com>
2024-07-08 14:46:15 -0500

    testing OrderStatusSelector

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -5,9 +5,12 @@ import userEvent from "@testing-library/user-event";
 
 describe("OrderStatusSelector", () => {
   const renderComponent = () => {
+    const onChange = vi.fn();
+
     render(
       <Theme>
-        <OrderStatusSelector onChange={vi.fn} />
+        <OrderStatusSelector onChange={onChange} />
+        {/* currently mock fxn is being passed here, but it needs to be defined so it can be accessed in test cases */}
       </Theme>
     );
 
@@ -18,6 +21,11 @@ describe("OrderStatusSelector", () => {
       // instead must use lazy evaluation which postpones execution of code until it's needed
       getOptions: () => screen.findAllByRole("option"),
       // must 1st be turned into a fxn
+      getOption: (label: RegExp) =>
+        screen.findByRole("option", { name: label }),
+      // takes a label as a regular expression
+      user: userEvent.setup(),
+      onChange,
     };
   };
 
@@ -27,9 +35,8 @@ describe("OrderStatusSelector", () => {
   });
 
   it("should render correct statuses", async () => {
-    const { trigger, getOptions } = renderComponent();
+    const { trigger, getOptions, user } = renderComponent();
 
-    const user = userEvent.setup();
     await user.click(trigger);
 
     const options = await getOptions();
@@ -37,5 +44,42 @@ describe("OrderStatusSelector", () => {
     expect(options).toHaveLength(3);
     const labels = options.map((option) => option.textContent);
     expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
+  });
+
+  it.each([
+    { label: /processed/i, value: "processed" },
+    { label: /fulfilled/i, value: "fulfilled" },
+  ])(
+    "should call onChange with $value when the $label option is selected",
+    async ({ label, value }) => {
+      // to get access to above properties, the argument must be destructured
+      const { trigger, user, onChange, getOption } = renderComponent();
+      await user.click(trigger);
+
+      const option = await getOption(label);
+      // using a filter to grab this particular option
+      await user.click(option);
+
+      expect(onChange).toHaveBeenCalledWith(value);
+    }
+  );
+  // convert this test to parameterized test to reuse...
+  // the "it" obj has a property called .each that is called and given an arr of objs
+  // when .each method is called, a fxn is returned
+
+  // have to handle "New" option differently bc since it is default value, when clicked, nothing happens...
+  // instead, we must select a different option then select "New" option
+  it("should call onChange with 'new' when the New option is selected", async () => {
+    const { trigger, user, onChange, getOption } = renderComponent();
+    await user.click(trigger);
+
+    const processedOption = await getOption(/processed/i);
+    await user.click(processedOption);
+
+    await user.click(trigger);
+    const newOption = await getOption(/new/i);
+    await user.click(newOption);
+
+    expect(onChange).toHaveBeenCalledWith("new");
   });
 });
