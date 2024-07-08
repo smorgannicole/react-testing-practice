f9608b5dbda111e95863ec9a26da40c2d7504242
Morgan <smorgannicole@gmail.com>
2024-07-08 13:55:03 -0500

    simplifying 'working with component libraries' test

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -4,42 +4,38 @@ import { Theme } from "@radix-ui/themes";
 import userEvent from "@testing-library/user-event";
 
 describe("OrderStatusSelector", () => {
-  it("should render new as the default value", () => {
+  const renderComponent = () => {
     render(
       <Theme>
         <OrderStatusSelector onChange={vi.fn} />
       </Theme>
     );
-    // Theme component provides styling/theming context needed by components from radix ui library. Without this context, OrderStatusSelector component may not render with correct styles/may not function properly
 
-    const selectorButton = screen.getByRole("combobox");
-    // there is no native html "select" element so we have to query for combobox (despite the element itself being a button) bc this component was pulled from a component library and has property of role="combobox"
-    expect(selectorButton).toHaveTextContent(/new/i);
+    return {
+      trigger: screen.getByRole("combobox"),
+      // options: screen.findAllByRole("option"),
+      // ^^ won't work bc renderComponent is executed when component is initially rendered (at which point there are no options rendered in the dom)...
+      // instead must use lazy evaluation which postpones execution of code until it's needed
+      getOptions: () => screen.findAllByRole("option"),
+      // must 1st be turned into a fxn
+    };
+  };
+
+  it("should render new as the default value", () => {
+    const { trigger } = renderComponent();
+    expect(trigger).toHaveTextContent(/new/i);
   });
 
   it("should render correct statuses", async () => {
-    // to verify this behavior, need to click select component. bc the options only appear after select component is clicked, they aren't rendered in dom ahead of time
-    render(
-      <Theme>
-        <OrderStatusSelector onChange={vi.fn} />
-      </Theme>
-    );
+    const { trigger, getOptions } = renderComponent();
 
-    const selectorButton = screen.getByRole("combobox");
     const user = userEvent.setup();
-    await user.click(selectorButton);
+    await user.click(trigger);
 
-    // now to find all the options... those options appear asynchronously so they have to be looked up using find method
-
-    const options = await screen.findAllByRole("option");
-    // test knows the elements are options by use of ARIA roles (options are typically given the role="option")
+    const options = await getOptions();
+    // this code is now executed after trigger is clicked and options are displayed
     expect(options).toHaveLength(3);
-    // now to make sure these options have correct labels...
-    // these options are html elements and html elements have a property called textContent which holds the label title...
-    // we can iterate over them and map them to their textContent
     const labels = options.map((option) => option.textContent);
-    // returns arr of labels
     expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
-    // unlikely that staus names will change in future
   });
 });
